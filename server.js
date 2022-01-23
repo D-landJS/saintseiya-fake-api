85c0b084b0e7d1b80d462a4ae90f2fc52d072d4e
D-LAND\Leon <d-landarrese80@hotmail.com>
2022-01-23 18:55:57 -0500

    fake-api-saintseiya

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./api/db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 
